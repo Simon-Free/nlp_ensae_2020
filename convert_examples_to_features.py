45977a98c631c56c5715e7d28dad464e381eaf42
Pierre Eustache <aquachoc97@gmail.com>
2020-04-12 03:24:04 +0200

    bert

--- convert_examples_to_features.py
+++ convert_examples_to_features.py
@@ -8,21 +8,6 @@ class InputFeatures(object):
         self.label_id = label_id
 
 
-def _truncate_seq_pair(tokens_a, tokens_b, max_length):
-    """Truncates a sequence pair in place to the maximum length."""
-
-    # This is a simple heuristic which will always truncate the longer sequence
-    # one token at a time. This makes more sense than truncating an equal percent
-    # of tokens from each, since if one sequence is very short then each token
-    # that's truncated likely contains more information than a longer sequence.
-    while True:
-        total_length = len(tokens_a) + len(tokens_b)
-        if total_length <= max_length:
-            break
-        if len(tokens_a) > len(tokens_b):
-            tokens_a.pop()
-        else:
-            tokens_b.pop()
 
 
 def convert_example_to_feature(example_row):
@@ -31,24 +16,13 @@ def convert_example_to_feature(example_row):
 
     tokens_a = tokenizer.tokenize(example.text_a)
 
-    tokens_b = None
-    if example.text_b:
-        tokens_b = tokenizer.tokenize(example.text_b)
-        # Modifies `tokens_a` and `tokens_b` in place so that the total
-        # length is less than the specified length.
-        # Account for [CLS], [SEP], [SEP] with "- 3"
-        _truncate_seq_pair(tokens_a, tokens_b, max_seq_length - 3)
-    else:
         # Account for [CLS] and [SEP] with "- 2"
-        if len(tokens_a) > max_seq_length - 2:
-            tokens_a = tokens_a[:(max_seq_length - 2)]
+    if len(tokens_a) > max_seq_length - 2:
+        tokens_a = tokens_a[:(max_seq_length - 2)]
 
     tokens = ["[CLS]"] + tokens_a + ["[SEP]"]
     segment_ids = [0] * len(tokens)
 
-    if tokens_b:
-        tokens += tokens_b + ["[SEP]"]
-        segment_ids += [1] * (len(tokens_b) + 1)
 
     input_ids = tokenizer.convert_tokens_to_ids(tokens)
 
@@ -66,12 +40,8 @@ def convert_example_to_feature(example_row):
     assert len(input_mask) == max_seq_length
     assert len(segment_ids) == max_seq_length
 
-    if output_mode == "classification":
-        label_id = label_map[example.label]
-    elif output_mode == "regression":
-        label_id = float(example.label)
-    else:
-        raise KeyError(output_mode)
+    
+    label_id = label_map[example.label]
 
     return InputFeatures(input_ids=input_ids,
                          input_mask=input_mask,
