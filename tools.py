8b6d86851011ab036739653ea601e1b8b70ce059
Pierre Eustache <aquachoc97@gmail.com>
2020-04-12 04:12:45 +0200

    Bert cleaned

--- tools.py
+++ tools.py
@@ -5,6 +5,7 @@ import os
 import sys
 import logging
 
+logging.basicConfig(filename='logs/runtime_log.log',level=logging.DEBUG)
 logger = logging.getLogger()
 csv.field_size_limit(2147483647) # Increase CSV reader's field limit incase we have long text.
 
@@ -14,19 +15,9 @@ class InputExample(object):
 
     def __init__(self, guid, text_a, text_b=None, label=None):
         """Constructs a InputExample.
-
-        Args:
-            guid: Unique id for the example.
-            text_a: string. The untokenized text of the first sequence. For single
-            sequence tasks, only this sequence must be specified.
-            text_b: (Optional) string. The untokenized text of the second sequence.
-            Only must be specified for sequence pair tasks.
-            label: (Optional) string. The label of the example. This should be
-            specified for train and dev examples, but not for test examples.
-        """
+"""
         self.guid = guid
         self.text_a = text_a
-        self.text_b = text_b
         self.label = label
 
 
@@ -46,6 +37,7 @@ class DataProcessor(object):
 
 
 class BinaryClassificationProcessor(DataProcessor):
+    """Processor for binary classification dataset."""
 
     def get_train_examples(self, data_dir):
         """See base class."""
@@ -70,5 +62,54 @@ class BinaryClassificationProcessor(DataProcessor):
             text_a = line[3]
             label = line[1]
             examples.append(
-                InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+                InputExample(guid=guid, text_a=text_a, label=label))
         return examples
+        
+class InputFeatures(object):
+    """A single set of features of data."""
+
+    def __init__(self, input_ids, input_mask, segment_ids, label_id):
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.label_id = label_id
+
+
+def convert_example_to_feature(example_row):
+    # return example_row
+    example, label_map, max_seq_length, tokenizer, output_mode = example_row
+
+    tokens_a = tokenizer.tokenize(example.text_a)
+
+        # Account for [CLS] and [SEP] with "- 2"
+    if len(tokens_a) > max_seq_length - 2:
+        tokens_a = tokens_a[:(max_seq_length - 2)]
+
+    tokens = ["[CLS]"] + tokens_a + ["[SEP]"]
+    segment_ids = [0] * len(tokens)
+
+
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+    # The mask has 1 for real tokens and 0 for padding tokens. Only real
+    # tokens are attended to.
+    input_mask = [1] * len(input_ids)
+
+    # Zero-pad up to the sequence length.
+    padding = [0] * (max_seq_length - len(input_ids))
+    input_ids += padding
+    input_mask += padding
+    segment_ids += padding
+
+    assert len(input_ids) == max_seq_length
+    assert len(input_mask) == max_seq_length
+    assert len(segment_ids) == max_seq_length
+
+    
+    label_id = label_map[example.label]
+
+    return InputFeatures(input_ids=input_ids,
+                         input_mask=input_mask,
+                         segment_ids=segment_ids,
+                         label_id=label_id)
+
